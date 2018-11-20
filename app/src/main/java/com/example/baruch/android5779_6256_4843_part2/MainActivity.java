620e61f0c7f7b396c3c21c4ca4cca904fdb17e71
gehlerb <gehlerb@gmail.com>
2018-11-20 19:47:20 +0200

    first commit

---
+++ app/src/main/java/com/example/baruch/android5779_6256_4843_part2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.baruch.android5779_6256_4843_part2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
