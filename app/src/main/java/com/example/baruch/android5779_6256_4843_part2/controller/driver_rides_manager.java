10f3150ec2390f3ed023803cd475b80b207226df
gehlerb <gehlerb@gmail.com>
2018-12-28 00:59:44 +0200

    navigation drawer first commit

--- app/src/main/java/com/example/baruch/android5779_6256_4843_part2/controller/driver_rides_manager.java
+++ app/src/main/java/com/example/baruch/android5779_6256_4843_part2/controller/driver_rides_manager.java
@@ -4,11 +4,15 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import com.example.baruch.android5779_6256_4843_part2.R;
 import com.example.baruch.android5779_6256_4843_part2.model.backend.Backend;
@@ -22,24 +26,47 @@ public class driver_rides_manager extends AppCompatActivity {
 
     //TODO move to fregment
 
-    ArrayList<Ride> rieds;
-    Backend backend;
-
+    private ArrayList<Ride> rieds;
+    private Backend backend;
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_rides_manager);
 
         startService(new Intent(this,NewRideService.class));
-
+        setMenu();
         loadFragment(new WaitingListFragment());
 
 
+
     }
+
+    private void setMenu() {
+        Toolbar toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
     private void loadFragment(Fragment fragment) {
         FragmentManager fm = getFragmentManager();
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
-        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
         fragmentTransaction.commit(); // save the changes
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
